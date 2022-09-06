3f301420ba27bc69b9d3ce226a2690088d320462
LeSaumon <sl.simonlebrun0@gmail.com>
2022-09-06 16:34:20 +0200

    MAJ

--- src/generate-npc-data.ts
+++ src/generate-npc-data.ts
@@ -1,6 +1,25 @@
 import puppeteer from 'puppeteer';
+const utf8 = require('utf8');
+import axios from 'axios';
 const WEBSITE_URL = 'http://www.npcgenerator.com/';
-import fs from "fs";
+const DEEPL_TRANSLATION_URL = 'https://api-free.deepl.com/v2/translate'
+// import fs from "fs";
+
+type TranslationBody = {
+    "text": string,
+    "target_lang": string
+}
+
+type TranslationData = {
+    "detected_source_language": string,
+    "text": string,
+}
+
+type TranslationResponse = {
+    "translations": TranslationData[],
+}
+
+
 
 const removeEscapeCharacter = (str) => {
     // Remove the trailing \n from a string if it exists
@@ -8,6 +27,31 @@ const removeEscapeCharacter = (str) => {
         return str.slice(0, -1);
     }
     return str;
+}
+
+async function translateData(requestBody) {
+    try {
+        const { data, status } = await axios.post<TranslationResponse>(
+            DEEPL_TRANSLATION_URL, requestBody,{
+                headers: {
+                    Accept: "application/json",
+                },
+            },
+        );
+
+        console.log('Response Status : ', status);
+        console.log(JSON.stringify(data, null, 4));
+        return data;
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log('error message: ', error.message);
+            return error.message;
+          } else {
+            console.log('unexpected error: ', error);
+            return 'An unexpected error occurred';
+          }
+    }
 }
 
 async function main() {
@@ -18,12 +62,18 @@ async function main() {
     // Get the NPC data from the page
     const description = await page.$$eval('div.npc-data > *', values => { return values.filter(value => value.innerText !== "#").map(value => value.innerText)})
     const noEscapeDescription = description.map(string => removeEscapeCharacter(string));
-
+    const encodedDescription = utf8.encode(noEscapeDescription);
+    const postRequestBody : TranslationBody = {
+        "target_lang": "FR",
+        "text": encodedDescription,
+    }
+    const translatedDescription = translateData(postRequestBody);
+    console.log(translatedDescription);
     // Create Data structure to send to DeepL API
     // Use UTF-8 encoded plain text
 
     // Put the NPC data into a file
-    fs.writeFileSync('npc-data.txt', noEscapeDescription.join('\n'));
+    // fs.writeFileSync('npc-data.txt', noEscapeDescription.join('\n'));
     await browser.close();
 }
 
