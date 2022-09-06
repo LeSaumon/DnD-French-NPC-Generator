e406065ba87cf0a048b08dadc402d17b3afcf5a9
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-09-06 16:24:47 +0200

    Added util file

--- src/generate-file/util.ts
+++ src/generate-file/util.ts
@@ -19,3 +19,21 @@ const fetcher = async (url: string, headers: AxiosRequestConfig, body: Translati
     const { data } = await axios.post<TranslationApiResponse>(url, body, headers);
     return data;
 }
+
+const translateToFrench = async (text: string) => {
+    const url = "https://api-free.deepl.com/v2/translate";
+    const apiKey = "YOUR_API_KEY";
+    const headers = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `DeepL-Auth-Key ${apiKey}`
+        }
+    }
+    const body : TranslationApiRequestBody = {
+        text: text,
+        target_lang: "FR",
+        source_lang: "EN"
+    }
+    const data = await fetcher(url, headers, body);
+    return data;
+}
