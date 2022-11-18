8496981b61e0da19c99b2d8ea7b6387c7a1bba5d
Saumon <simon.lebrun@sharingcloud.com>
2022-11-18 17:32:20 +0100

    Added translation

--- src/translator/translator.ts
+++ src/translator/translator.ts
@@ -1,22 +1,58 @@
 import * as deepl from 'deepl-node';
-import { Abilities, NpcData } from '../types/Npc.js';
+import { NpcData } from '../types/Npc.js';
 
 const translator: deepl.Translator = new deepl.Translator(
   process.env.DEEPL_API_KEY,
 );
-const targetLang: deepl.TargetLanguageCode = 'fr'
-export const translateDescription = async (description: NpcData["description"]) => {
-
-  const translatedDescription: string = (await translator.translateText(description, null, targetLang)).text;
-  return translatedDescription
+const targetLang: deepl.TargetLanguageCode = 'fr';
+export const translateDescription = async (
+  description: NpcData['description'],
+) => {
+  const translatedDescription: string = (
+    await translator.translateText(description, null, targetLang)
+  ).text;
+  return translatedDescription;
 };
 
-export const translatePersonnality = async (description: NpcData["personnality"]) => {
-
-  const translatedPersonnality: string = (await translator.translateText(description, null, targetLang)).text;
-  return translatedPersonnality
+export const translatePersonnality = async (
+  description: NpcData['personnality'],
+) => {
+  const translatedPersonnality: string = (
+    await translator.translateText(description, null, targetLang)
+  ).text;
+  return translatedPersonnality;
 };
 
-export const translateAbilities = async (abilities: Abilities) => {
+// export const translateAbilities = async (abilities: Abilities): Abilities => {
+//   const iterator = Object.keys(abilities);
+//   const translatedAbilities = {
+//     strength: await translator.translateText(
+//       abilities.strength,
+//       null,
+//       targetLang,
+//     ).text,
+//     dexterity: await translator.translateText(
+//       abilities.dexterity,
+//       null,
+//       targetLang,
+//     ),
+//     constitution: await translator.translateText(
+//       abilities.constitution,
+//       null,
+//       targetLang,
+//     ),
+//     intellect: await translator.translateText(
+//       abilities.intellect,
+//       null,
+//       targetLang,
+//     ),
+//     wisdom: await translator.translateText(abilities.wisdom, null, targetLang),
+//     charisma: await translator.translateText(
+//       abilities.charisma,
+//       null,
+//       targetLang,
+//     ),
+//   };
 
-}
+//   return await translatedAbilities;
+// };
